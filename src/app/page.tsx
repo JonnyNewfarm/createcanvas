af89848603fbeb9fe9fa6b6a2e3cd2609cedba83
Jonas Nygaard <jonasnygaard96@gmail.com>
2024-11-20 02:33:23 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
 import Container from "@/components/Container";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-[#E4E2DD]">
-      <section>
-        <Container
-          className="pb-24 pt-10 lg:grid lg:grid-cols-3
-        sm:pb-2 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52"
-        >
-          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-              <div className="absolute w-64 left-0 -top-20 hidden lg:block">
-                <img src="/splashBrush.png" alt="" className="border-none" />
-              </div>
-            </div>
-          </div>
-        </Container>
-      </section>
+    <div className="h-screen flex xlgap-x-28 flex-col-reverse md:flex-row items-center justify-center z-10 w-full bg-[#b8c6e0] px-20">
+      <div className="flex flex-col -[#0E0D12]">
+        <h1 className="font-extrabold text-[40px]">Pulvinar ac</h1>
+        <p className="font-extrabold text-[40px] mt-[-15px]">
+          Sed auctor felis
+        </p>
+        <p className="text-xl">Aenean in ipsum et</p>
+        <p className="text-xl">Commodo orci </p>
+        <div className="w-36">
+          <Link
+            className="flex justify-center rounded-l py-2 text-[18px] text-white bg-[#0E0D12]"
+            href={""}
+          >
+            Create now
+          </Link>
+        </div>
+      </div>
+      <div className="w-[630px]">
+        <img className="object-cover" src="/heroImage.jpg" />
+        <h1 className="absolute ml-[200px] scale-y-150  text-[30px] font-extrabold bottom-[365px] text-stone-50">
+          Dapi convallis
+        </h1>
+        <h1 className="absolute bottom-[312px] ml-[265px] font-semibold text-stone-50">
+          Maecenas es
+        </h1>
+      </div>
     </div>
   );
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ const Container = ({
   className,
   children,
 }: {
-  className: string;
+  className?: string;
   children: ReactNode;
 }) => {
   return (
